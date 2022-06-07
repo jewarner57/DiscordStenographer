7b5eecce47a52a274ac8a67f29a0e78ab9aec02b
= <jewarner57@gmail.com>
2022-06-06 23:14:15 -0400

    initial commit

---
+++ src/onClientMessage.js
@@ -0,0 +1,29 @@
+const { setRole, setHome, graphActivity } = require('./commands')
+
+function onClientMessage(message) {
+    // if the message isnt meant for this bot then ignore it
+    if (message.content.substring(0, 1) !== '~') {
+        return
+    }
+
+    const commandArguments = message.content.toLowerCase().split(" ")
+    if (commandArguments.length < 1) {
+        message.channel.send('Command Not Found.')
+    }
+
+    switch (commandArguments[0]) {
+        case '~setrole':
+            setRole(message)
+            break;
+        case '~sethome':
+            setHome(message)
+            break;
+        case '~graph-activity':
+            graphActivity(message)
+            break;
+        default:
+            message.channel.send('Command Not Found.')
+    }
+}
+
+exports.onClientMessage = onClientMessage
